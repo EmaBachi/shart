1ac1ee0f27088603d83c8a9f1775a09830dc2397
Emanuele Bachi <emanuele.bachi@studenti.polito.it>
2017-11-01 09:58:14 +0100

    Developed footer. Real link on Register page

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ from flask import Flask, render_template
 # Instantiate application object
 app = Flask(__name__)
 
+
+# Route for home
 @app.route('/')
 def index():
     return render_template('home.html')
+
+
+# Route for register a new user. Register endpoint can be reached by GET and POST requests
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
 
 
 # Check name of application
