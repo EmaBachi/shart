ceddfcc129d01c61a7488de55d779f47e1f09d13
Emanuele Bachi <emanuele.bachi@studenti.polito.it>
2018-03-10 17:46:28 +0100

    Connection string of mongo atlas

--- Repositories.py
+++ Repositories.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
 
-# "mongodb://emaBachi:1LGr3co1994?@shart-shard-00-00-q1do7.mongodb.net:27017,shart-shard-00-01-q1do7.mongodb.net:27017,shart-shard-00-02-q1do7.mongodb.net:27017/test?ssl=true&replicaSet=shart-shard-0&authSource=admin"
+# "mongodb://emaBachi:mySecretPassord@shart-shard-00-00-q1do7.mongodb.net:27017,shart-shard-00-01-q1do7.mongodb.net:27017,shart-shard-00-02-q1do7.mongodb.net:27017/test?ssl=true&replicaSet=shart-shard-0&authSource=admin"
 
 
-client = MongoClient(maxPoolSize=5)
+client = MongoClient("mongodb://emaBachi:mySecretPassord@shart-shard-00-00-q1do7.mongodb.net:27017,shart-shard-00-01-q1do7.mongodb.net:27017,shart-shard-00-02-q1do7.mongodb.net:27017/test?ssl=true&replicaSet=shart-shard-0&authSource=admin")
 db = client.shart
 
 
